65beed0ba194d4640381ab0d831ee91f04838029
Will Fong <willfong@hey.com>
2022-01-08 15:56:13 +0800

    Prettier

--- index.js
+++ index.js
@@ -5,54 +5,54 @@ const { promisify } = require("util");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const REDIS_HOST = process.env.REDIS || 'localhost';
+const REDIS_HOST = process.env.REDIS || "localhost";
 
 app.use(morgan("combined"));
 app.use(express.static("public"));
 app.use(express.static("dist"));
-app.use(express.json())
+app.use(express.json());
 
-const redisClient = redis.createClient({host: REDIS_HOST});
-redisClient.on("error", function(error) {
-    console.error(error);
+const redisClient = redis.createClient({ host: REDIS_HOST });
+redisClient.on("error", function (error) {
+	console.error(error);
 });
 const redisGet = promisify(redisClient.get).bind(redisClient);
 const redisSet = promisify(redisClient.set).bind(redisClient);
 const redisDbSize = promisify(redisClient.dbsize).bind(redisClient);
 
 app.post("/new", async (req, res) => {
-    const id = Math.floor((Math.random() * 9000) + 1000);
-    const bin = req.body.bin;
-    await redisSet(id, bin, 'EX', 60);
-    res.json({id});
+	const id = Math.floor(Math.random() * 9000 + 1000);
+	const bin = req.body.bin;
+	await redisSet(id, bin, "EX", 60);
+	res.json({ id });
 });
 
 app.get("/get", async (req, res) => {
-    const id = req.query.id;
-    if (!id || !parseInt(id, 10)) {
-        return res.status(400).send("400: Invalid ID");
-    }
-    const response = await redisGet(id);
-    if (!response) {
-        return res.status(404).send("404: Expired or invalid ID")
-    }
-    res.send(response);
+	const id = req.query.id;
+	if (!id || !parseInt(id, 10)) {
+		return res.status(400).send("400: Invalid ID");
+	}
+	const response = await redisGet(id);
+	if (!response) {
+		return res.status(404).send("404: Expired or invalid ID");
+	}
+	res.send(response);
 });
 
 app.get("/size", async (req, res) => {
-    const response = await redisDbSize();
-    res.json({size: response});
+	const response = await redisDbSize();
+	res.json({ size: response });
 });
 
-app.use(function(req, res) {
-    res.status(404).send("404: Page not found");
+app.use(function (req, res) {
+	res.status(404).send("404: Page not found");
 });
 
-app.use(function(error, req, res, next) {
-    console.log(error);
-    res.status(500).send("500: Internal server error");
+app.use(function (error, req, res, next) {
+	console.log(error);
+	res.status(500).send("500: Internal server error");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+	console.log(`Server started on port: ${PORT}`);
 });
--- public/bin.js
+++ public/bin.js
@@ -4,20 +4,20 @@ const binContents = document.getElementById("binContents");
 const newBinCode = document.getElementById("newBinCode");
 
 btnNewBin.onclick = async (event) => {
-    try {
-        const response = await axios.post("/new", {bin: binContents.value});
-        newBinCode.innerHTML = `Your code is: ${response.data.id}`;
-    } catch(err) {
-        console.log(err);
-    }
-}
+	try {
+		const response = await axios.post("/new", { bin: binContents.value });
+		newBinCode.innerHTML = `Your code is: ${response.data.id}`;
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 btnShowBin.onclick = async (event) => {
-    const id = document.getElementById("code");
-    try {
-        const response = await axios.get("/get", {params: {id: id.value}});
-        document.getElementById("codeResults").innerHTML = response.data;
-    } catch(err) {
-        console.log(err);
-    }
-}
+	const id = document.getElementById("code");
+	try {
+		const response = await axios.get("/get", { params: { id: id.value } });
+		document.getElementById("codeResults").innerHTML = response.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
