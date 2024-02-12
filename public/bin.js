df595d071f866e933cac01fd6d6df78edd690fd1
Will Fong <willfong@hey.com>
2024-02-12 12:47:53 +0700

    Add copy to clipboard

--- public/bin.js
+++ public/bin.js
@@ -1,32 +1,47 @@
 const btnNewBin = document.getElementById("btnNewBin");
 const btnShowBin = document.getElementById("btnShowBin");
+const btnCopyBin = document.getElementById("btnCopyBin");
+const binNewContent = document.getElementById("binNewContent");
 const binContents = document.getElementById("binContents");
 const newBinCode = document.getElementById("newBinCode");
+const codeId = document.getElementById("code");
+const binResults = document.getElementById("binResults");
 
 btnNewBin.onclick = async (event) => {
 	try {
-		const response = await axios.post("/new", { bin: binContents.value });
+		const response = await axios.post("/new", { bin: binNewContent.value });
 		newBinCode.innerHTML = `Your code is: ${response.data.id}`;
 	} catch (err) {
 		console.log(err);
+		newBinCode.innerHTML = `Error: ${err.response.data}`;
 	}
 };
 
 btnClearBin.onclick = async (event) => {
 	try {
-		binContents.value = "";
+		binNewContent.value = "";
 	} catch (err) {
 		console.log(err);
 	}
 };
 
 
-btnShowBin.onclick = async (event) => {
-	const id = document.getElementById("code");
+btnCopyBin.onclick = async (event) => {
+	const textToCopy = binContents.innerText;
 	try {
-		const response = await axios.get("/get", { params: { id: id.value } });
-		document.getElementById("codeResults").innerHTML = response.data;
+	  await navigator.clipboard.writeText(textToCopy);
+	} catch (err) {
+	  console.error('Failed to copy text', err);
+	}
+};
+
+btnShowBin.onclick = async (event) => {
+	binResults.classList.toggle('hidden');
+	try {
+		const response = await axios.get("/get", { params: { id: codeId.value } });
+		binContents.innerHTML = response.data;
 	} catch (err) {
 		console.log(err);
+		binContents.innerHTML = err.response.data;
 	}
 };
