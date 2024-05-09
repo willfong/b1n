75b79ef1eaea66c13709e7654867b7280759f342
Will Fong <willfong@hey.com>
2024-05-09 17:21:30 +0700

    lint + bug with < in the text for tags

--- public/bin.js
+++ public/bin.js
@@ -8,40 +8,39 @@ const codeId = document.getElementById("code");
 const binResults = document.getElementById("binResults");
 
 btnNewBin.onclick = async (event) => {
-	try {
-		const response = await axios.post("/new", { bin: binNewContent.value });
-		newBinCode.innerHTML = `Your code is: ${response.data.id}`;
-	} catch (err) {
-		console.log(err);
-		newBinCode.innerHTML = `Error: ${err.response.data}`;
-	}
+  try {
+    const response = await axios.post("/new", { bin: binNewContent.value });
+    newBinCode.innerHTML = `Your code is: ${response.data.id}`;
+  } catch (err) {
+    console.log(err);
+    newBinCode.innerHTML = `Error: ${err.response.data}`;
+  }
 };
 
 btnClearBin.onclick = async (event) => {
-	try {
-		binNewContent.value = "";
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    binNewContent.value = "";
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-
 btnCopyBin.onclick = async (event) => {
-	const textToCopy = binContents.innerText;
-	try {
-	  await navigator.clipboard.writeText(textToCopy);
-	} catch (err) {
-	  console.error('Failed to copy text', err);
-	}
+  const textToCopy = binContents.innerText;
+  try {
+    await navigator.clipboard.writeText(textToCopy);
+  } catch (err) {
+    console.error("Failed to copy text", err);
+  }
 };
 
 btnShowBin.onclick = async (event) => {
-	binResults.classList.toggle('hidden');
-	try {
-		const response = await axios.get("/get", { params: { id: codeId.value } });
-		binContents.innerHTML = response.data;
-	} catch (err) {
-		console.log(err);
-		binContents.innerHTML = err.response.data;
-	}
+  binResults.classList.toggle("hidden");
+  try {
+    const response = await axios.get("/get", { params: { id: codeId.value } });
+    binContents.textContent = response.data;
+  } catch (err) {
+    console.log(err);
+    binContents.textContent = err.response.data;
+  }
 };
